f67a47eb06c84f64e00ccc0c0030d0148e224d32
parikshitshelorkar <parikshitshelorkar2.0@gmail.com>
2026-04-09 18:50:52 +0530

    db changes

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '@parikshit123',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'finance_tracker',
   waitForConnections: true,
   connectionLimit: 10,
